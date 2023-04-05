92e1a94150ca65107ce1b061078eee23148a2358
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-04-05 11:05:24 +0700

    memasukkan struktur Detail Alamat ke dalam Struktur Mahasiswa

--- Structur2/Structur2.cpp
+++ Structur2/Structur2.cpp
@@ -1,20 +1,34 @@
-// Structur2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+struct DetailAlamat
 {
-    std::cout << "Hello World!\n";
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukkan NIM : ";
+	cin.getline(mhs.nim, 12);
+	cout << "Masukkan Umur : ";
+	cin >> mhs.umur;
+	cin.ignore(1, '\n');
+	cout << "Masukkan Nama : ";
+	cin.getline(mhs.nama, 20);
+	cout << "Alamat : ";
+	cout << "\n\tMasukkan Desa : ";
+	cin.getline(mhs.alamat.desa, 20);
+	cout << "\tMasukkan Kota : ";
+	cin.getline(mhs.alamat.kota, 20);
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
